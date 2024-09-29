67d3fa3aa2666dd3cdcfe28b531b0563f248bea0
Christine Ng <christineng1028@gmail.com>
2024-09-29 16:56:08 -0400

    dark and light mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { useTheme } from "./ThemeProvider";
+import { ThemeToggle } from "./components/ThemeToggle";
 
 function App() {
+  const { darkMode } = useTheme();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-sm">
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${darkMode ? "dark" : ""}`}>
+      <ThemeToggle />
     </div>
   );
 }
---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,24 @@
+import { useTheme } from "../ThemeProvider";
+import SunIcon from "../assets/icons/sun.svg";
+import MoonIcon from "../assets/icons/moon.svg";
+
+export const ThemeToggle = () => {
+  const { darkMode, toggleDarkMode } = useTheme();
+
+  return (
+    <button
+      onClick={toggleDarkMode}
+      className={`fixed top-4 right-4 border-2 rounded-full border-dotted w-12 h-12 flex items-center justify-center transition-shadow duration-300 hover:shadow-[0_0_10px_rgba(0,0,0,0.3)] ${
+        darkMode
+          ? "border-white hover:shadow-[0_0_10px_rgba(255,255,255,0.5)]"
+          : "border-[rgba(0,0,0,0.8)] hover:shadow-[0_0_10px_rgba(0,0,0,0.5)]"
+      }`}
+    >
+      <img
+        src={darkMode ? SunIcon : MoonIcon}
+        alt="Toggle Theme"
+        className="w-5 h-5"
+      />
+    </button>
+  );
+};
